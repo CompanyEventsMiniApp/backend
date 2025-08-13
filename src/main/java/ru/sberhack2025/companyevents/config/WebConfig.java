57987c9720b170115bf630236907f4bf2ca7a323
Olga Iakubova <87225985+OlgaRhythm@users.noreply.github.com>
2025-08-13 18:26:25 +0300

    Изменены настройки corps

--- src/main/java/ru/sberhack2025/companyevents/config/WebConfig.java
+++ src/main/java/ru/sberhack2025/companyevents/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "https://companyeventsminiapp.github.io", // GitHub Pages
-                    "https://jakuba.olgarhythm.ru/company-events/", // Фронтенд
+                    "https://jakuba.olgarhythm.ru", // Фронтенд
                     "https://web.telegram.org", // Telegram Web App
                     "http://localhost:3000" // Для локальной разработки
                 )
